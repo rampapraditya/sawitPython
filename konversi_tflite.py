b085bbb5316d0453c97cb4e0ea01b16b87c537f4
rampapraditya <rampapraditya@gmail.com>
2024-10-02 10:30:17 +0700

    revisi

---
+++ konversi_tflite.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+# Path ke model yang telah dilatih
+model_save_path = 'models/palm_maturity_model.keras'
+tflite_model_save_path = 'models/model.tflite'
+
+# Memuat model TensorFlow yang telah dilatih
+model = tf.keras.models.load_model(model_save_path)
+
+# Membuat converter untuk mengonversi model ke format TFLite
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+# Mengonversi model ke format TFLite
+tflite_model = converter.convert()
+
+# Menyimpan model TFLite ke file
+with open(tflite_model_save_path, 'wb') as f:
+    f.write(tflite_model)
+
+print(f"Model berhasil dikonversi ke TFLite dan disimpan di: {tflite_model_save_path}")
